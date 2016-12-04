598317450bed8bb26b35b9ea048f4f2cb8e5ae20
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2016-12-04 21:37:32 +0000

    disable tracing

--- runtime/tracing.h
+++ runtime/tracing.h
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define WITH_TRACING 1
+#define WITH_TRACING 0
 
 struct item_t {
     long tick;
@@ -24,9 +24,15 @@ struct event_tracer {
     struct item_t items[CHUNK_SIZE];
 };
 
+#if WITH_TRACING
 #define TRACER_RECORD0(w,e)      record((w)->l->event_tracer,e,0,0)
 #define TRACER_RECORD1(w,e,a)    record((w)->l->event_tracer,e,(__intptr_t)(a),0)
 #define TRACER_RECORD2(w,e,a,b)  record((w)->l->event_tracer,e,(__intptr_t)(a),(__intptr_t)(b))
+#else
+#define TRACER_RECORD0(w,e)
+#define TRACER_RECORD1(w,e,a)
+#define TRACER_RECORD2(w,e,a,b)
+#endif
 
 typedef struct event_tracer event_tracer_t;
 
